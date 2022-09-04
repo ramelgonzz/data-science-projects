e38356d4c271b3d3df6bee7c3aa1d90abfa4f212
R <ramelgonzz@gmail.com>
2022-09-04 09:32:05 -0500

    initialize project

---
+++ uncanny/main.py
@@ -0,0 +1,4 @@
+#A project for classifying human faces with their resemblance to uncanny valley or ‘humanness’
+#Sources:
+#https://www.quora.com/Will-it-be-possible-to-use-a-neural-network-to-determine-if-something-is-in-the-Uncanny-Valley
+#https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3725829/
